3f1845fff782afcf35e5698c1006c8b4fbbce29c
Sanjeev Giri <girisanjib191@gmail.com>
2025-09-05 12:42:19 +0545

    cors

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const cors = require("cors");
 dotenv.config();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/user", userRoutes);
-app.use(cors());
 
 mongoose
   .connect(process.env.MONGO_URI)
